f83558e577cbf9e17716322e0f64f08aa09df5ef
Matheus Ligieri <matheusligieri@gmail.com>
2021-01-07 17:50:42 -0300

    changes

--- api/routes/person.js
+++ api/routes/person.js
@@ -2,5 +2,6 @@ module.exports = app => {
     const controller = app.controllers.person
     app.route('/api/v1/person')
     .get(controller.listPersons)
+    //POST
     .post(controller.createPersons)
 }
