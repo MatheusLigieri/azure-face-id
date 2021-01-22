965e7c647819c04483fe5b0180c9a8e41522f427
Matheus Ligieri <matheusligieri@gmail.com>
2021-01-22 16:06:22 -0300

    feature/person post and get

--- api/controllers/person.js
+++ api/controllers/person.js
@@ -1,24 +1,19 @@
 module.exports = app => {
   // const personDB = app.data.person
-  const pDB = require('../../db/queries')
+  const Person = require('../../db/person')
   const controller = {};
 
-  controller.listPersons = (req, res) => res.status(200).json(pDB.getAllPerson());
+  controller.listPersons = (req, res) => {
+    Person.GET(res)
+  }
 
-  // controller.createPerson = (req, res) => {
-  //   personMock.data.push({
-  //     id: uuidv4(),
-  //     parentId: uuidv4(),
-  //     name: req.body.name,
-  //     birthDate: req.body.birthDate,
-  //     cellphone: req.body.cellphone,
-  //     phone: req.body.phone,
-  //     email: req.body.email,
-  //     ocupation: req.body.ocupation,
-  //     state: req.body.state,
-  //   });
-  //   res.status(200).json(personDB)
-  // }
+  controller.createPerson = (req, res) => {
+    var payload = req.body
+    console.log(payload)
+    console.log('typeof')
+    console.log(typeof(payload))
+    Person.POST(payload, res)
+  }
 
   return controller;
 }
